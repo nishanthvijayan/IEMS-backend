3d804ec49e1dfb2b0790e2623f2ea8b827346c78
Nishanth Vijayan <nishanththegr8@gmail.com>
2016-04-26 12:28:55 +0530

    Improve transaction and guest_user model test

--- test/models/guest_user_test.rb
+++ test/models/guest_user_test.rb
@@ -9,9 +9,10 @@ class GuestUserTest < ActiveSupport::TestCase
     assert @guest_user.valid?
   end
 
-  test 'should respond to regular_user and host_user' do
+  test 'should respond to regular_user and host_user and transactions' do
     assert_respond_to @guest_user, :regular_user
     assert_respond_to @guest_user, :host_user
+    assert_respond_to @guest_user, :transactions
   end
 
   test 'name should not be blank' do
@@ -26,6 +27,11 @@ class GuestUserTest < ActiveSupport::TestCase
 
   test 'name should not be too short' do
     @guest_user.name = 'abc'
+    assert_not @guest_user.valid?
+  end
+
+  test 'regular_user should be set' do
+    @guest_user.regular_user = nil
     assert_not @guest_user.valid?
   end
 
--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class TransactionTest < ActiveSupport::TestCase
   def setup
     @regular_transaction = Transaction.new(food_type: 'D', regular_user_id: 1, date: 'Wed, 7 Jun 2015', price: 50)
+    @guest_user = guest_users(:guest_one)
   end
 
   test 'should be valid' do
@@ -36,5 +37,17 @@ class TransactionTest < ActiveSupport::TestCase
   test 'guest_user_id should not be blank for guest transaction' do
     @regular_transaction.guest_transaction = true
     assert_not @regular_transaction.valid?
+  end
+
+  test 'responds to set_billed_user' do
+    assert_respond_to @regular_transaction, :set_billed_user
+  end
+
+  test 'sets correct regular_user if guest_transaction' do
+    @regular_transaction.guest_transaction = true
+    @regular_transaction.regular_user = nil
+    @regular_transaction.guest_user = @guest_user
+    @regular_transaction.save
+    assert_not @regular_transaction.regular_user_id.nil?
   end
 end
